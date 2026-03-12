5dd26447f6134d5169b3c70c858de2ba9b095168
kwonjiwon <kjw81024@gmail.com>
2026-03-12 17:57:01 +0900

    feature: file upload, download response

---
+++ src/main/java/com/example/membermanagement/dto/FileUploadResponse.java
@@ -0,0 +1,10 @@
+package com.example.membermanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FileUploadResponse {
+    private final String key;
+}
